59ce357282223f56f63c8f405845af89f788226a
Matt Ross <matt@mattross.io>
2015-07-06 11:30:15 -0400

    Update git aliases to not conflict with plugins

--- zsh/custom/git.aliases.zsh
+++ zsh/custom/git.aliases.zsh
@@ -1,29 +1,30 @@
-alias g='git'
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
+# alias g='git'
+# alias ga='git add'
+# alias gb='git branch'
+# alias gc='git commit -v'
+# alias gca='git commit -v -a'
 alias gci='git commit --interactive'
-alias gcl='git clone'
-alias gco='git checkout'
-alias gdv='git diff -w "$@" | vim -R -'
+alias gcl='git clone --recursive'
+# alias gco='git checkout'
+# alias gdv='git diff -w "$@" | vim -R -'
 alias gfgr='git fetch && git rebase'
-alias gits="git status"
-alias gl='git log --graph --abbrev-commit'
-alias gll='gl --pretty=oneline'
-alias glg="gl --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --date=relative"
-alias glgs="glg --stat"
-alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
+# alias gits="git status"
+# alias gl='git log --graph --abbrev-commit'
+# alias gll='gl --pretty=oneline'
+# alias glg="gl --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --date=relative"
+# alias glgs="glg --stat"
+# alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
 alias gpl='git pull'
-alias gplo='gpl origin'
-alias gplh='gpl heroku'
-alias gplpu='gpl && git push'
-alias gplr='gpl --rebase'
+alias gplo='git pull origin'
+alias gplh='git pull heroku'
+# alias gplpu='gpl && git push'
+# alias gplr='gpl --rebase'
 alias gps='git push'
-alias gpsh='gps heroku'
-alias gpso='gps origin'
+alias gpsh='git push heroku'
+alias gpso='git push origin'
 alias gs='git status'
-alias gss='gs -s'
-alias gw="git whatchanged"
-alias gd="git diff"
-alias gds="gd --staged"
+# alias gss='gs -s'
+# alias gw="git whatchanged"
+# alias gd="git diff"
+alias gds="git diff --staged"
+alias gdws="git diff --word-diff --staged"
