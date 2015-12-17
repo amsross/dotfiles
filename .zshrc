ac990a955705ab5af7a6ef4439a305d262de0481
Matt Ross <matt@mattross.io>
2015-12-17 09:01:06 -0500

    Update zsh config

--- .zshrc
+++ .zshrc
@@ -1,4 +1,5 @@
 # Path to your oh-my-zsh installation.
+export LANG=en_US.UTF-8
 export TERM=xterm-256color
 export ZSH=$HOME/.oh-my-zsh
 export VIMBLACKLIST='somefakeplugin'
--- zsh/custom/general.aliases.zsh
+++ zsh/custom/general.aliases.zsh
@@ -5,6 +5,7 @@ if grep --color "a" <<<"a" &>/dev/null; then
 fi
 
 # Directory
+CLICOLOR=1
 LS_COLORS='di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
 export LS_COLORS
 alias ls='ls -hBGlah -F'
